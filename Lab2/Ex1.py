31d822fc8a54764c122b048f87ba8bc62db67a79
rnkazman <kazman@hawaii.edu>
2024-09-04 13:28:48 -1000

    Exercise 1

---
+++ Lab2/Ex1.py
@@ -0,0 +1,10 @@
+# Ask the user to enter a number between 1 and 100.  Square the number
+# and return the value to the user.
+# Name: Rick Kazman
+# Date created: 9/4/24
+
+value_entered = input("Please enter an integer between 1 and 100: ")
+
+value_entered_int = int(value_entered)
+value_squared = value_entered_int**2
+print ("The value squared= ", value_squared)
---
+++ Lab2/Example1.py
@@ -0,0 +1,15 @@
+# This program is to explore data types
+# Author: Rick Kazman
+# Date: 9/4/24
+
+trip_id = "s8dl3j4ls2"
+print("The type of trip_id is:", type(trip_id))
+
+trip_seconds = 300;
+print("The type of trip_seconds is:", type(trip_seconds))
+
+trip_fare = 6.75;
+print("The type of trip_fare is:", type(trip_fare))
+
+trip_complete = True;
+print("The type of trip_complete is:", type(trip_complete))
