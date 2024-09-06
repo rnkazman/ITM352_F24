7af29c0e694187b652a9cb024d160c99a386170a
rnkazman <kazman@hawaii.edu>
2024-09-06 12:29:42 -1000

    Initial versions of Exercises 2-4

---
+++ Lab2/Ex2.py
@@ -0,0 +1,14 @@
+# Get the user's birth year and subtract the current year to 
+# get their current age.
+
+birth_year = input("Please enter your four digit birth year: ")
+birth_year = int(birth_year)
+
+# This should be changed.  We should not hard-code the year.
+current_year = 2024
+
+# This doesn't take into account the month.  Need to fix.
+age = current_year - birth_year
+
+print("You entered: ", birth_year)
+print("Your age is:", age)
---
+++ Lab2/Ex4.py
@@ -0,0 +1,10 @@
+# Ask the user to enter a floating point number between 1 and 100.  Square the number
+# and return the value to the user.
+# Name: Rick Kazman
+# Date created: 9/6/24
+
+value_entered = input("Please enter an floating point number between 1 and 100: ")
+
+value_entered = float(value_entered)
+value_squared = value_entered**2
+print ("The value squared= ", round(value_squared,2))
