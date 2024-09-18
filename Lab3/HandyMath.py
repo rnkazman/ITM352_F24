9856d280b09da1421aa4c0a87c28476cb8e0bc4a
rnkazman <kazman@hawaii.edu>
2024-09-18 10:01:10 -1000

    Minor updates

--- Lab3/HandyMath.py
+++ Lab3/HandyMath.py
@@ -4,3 +4,5 @@
 def midpoint(num1, num2):
     return ((num1 + num2)/2)
 
+def exponent(num1, num2):
+    return (num1 ** num2)
--- Lab3/UseModule.py
+++ Lab3/UseModule.py
@@ -8,3 +8,8 @@ number1 = float(number1)
 
 mid = HM.midpoint(number1, number2)
 print("The midpoint is: ", mid)
+
+exp = HM.exponent(number1, number2)
+print(f"The value of raising {number1} to the power of {number2} is: {exp}")
+
+print (HM.exponent.__name__)
