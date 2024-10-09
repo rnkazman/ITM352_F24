01bb430bcd1a2f8339fd1349f3028e8a63e99872
rnkazman <kazman@hawaii.edu>
2024-10-09 09:30:47 -1000

    Update Ex1.py

--- Lab8/Ex1.py
+++ Lab8/Ex1.py
@@ -6,6 +6,6 @@ product = {
 
 tax_rate = 0.045
 
-total = product["price"] + product["price"] * tax_rate
+total = round(product["price"] + product["price"] * tax_rate, 2)
 
-print(f"A {product["name"]} costs ${round(total,2)}")
+print(f"A {product["name"]} costs ${total:.2f}")
