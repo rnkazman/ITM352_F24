0f50e923f95612a702158d7dd44ae339bd890954
rnkazman <kazman@hawaii.edu>
2024-11-20 13:31:30 -1000

    Update Ex7.py

--- Lab14/Ex7.py
+++ Lab14/Ex7.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
+import numpy as np
 
-trips_df = pd.read_json("taxi trips Fri 7_7_2017.csv")
+trips_df = pd.read_csv("taxi trips Fri 7_7_2017.csv")
 
 trips_df = trips_df[['dropoff_community_area', 'pickup_community_area']]
 trips_freq = pd.crosstab(trips_df.pickup_community_area, \
@@ -15,7 +16,11 @@ trips_freq = trips_freq.pivot(index="pickup_community_area",
                 columns="dropoff_community_area", 
                 values="numtrips")
 
+
+trips_freq = trips_freq.replace(np.nan, 0)
+trips_freq = trips_freq.astype(int)
 fig = plt.figure()
 
-ax = sns.heatmap(trips_freq, annot=True, fmt="d", cmap="Yl0rRd")
+ax = sns.heatmap(trips_freq, annot=True, fmt= "d", cmap="YlOrRd")
+
 plt.show()
